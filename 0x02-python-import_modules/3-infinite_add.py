343f038395995b34ca2a34ec3387de46f5326f1e
“Riskallah9” <riskallah9housni@gmail.com>
2024-01-04 04:13:29 -0800

    Tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+ __name__ == "__main__":
+ import sys
+ sum = 0
+ for i in range(len(sys.argv) - 1):
+     sum += int(sys.argv[i + 1])
+     print(sum)
