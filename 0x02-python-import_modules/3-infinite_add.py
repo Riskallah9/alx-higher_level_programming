acf8e398d2dff61194523337636ef8ec88a1ce32
“Riskallah9” <riskallah9housni@gmail.com>
2024-01-04 05:18:39 -0800

    Tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
- __name__ == "__main__":
- import sys
- sum = 0
- for i in range(len(sys.argv) - 1):
-     sum += int(sys.argv[i + 1])
-     print(sum)
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
