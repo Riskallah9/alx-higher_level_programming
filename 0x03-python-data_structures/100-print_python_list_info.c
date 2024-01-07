85909374ff2c4939ed16e841916998019e8e2107
“Riskallah9” <riskallah9housni@gmail.com>
2024-01-07 10:07:48 -0800

    apdate

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,30 +1,18 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <Python.h>
+/**
+ * print_python_list_info -  function that prints some basic
+ *							info about Python lists
+ * @p: python list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int elem;
 
-void print_python_list_info(PyObject *p) {
-    if (!PyList_Check(p)) {
-        printf("Object is not a Python List.\n");
-        return;
-    }
-
-    printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
-    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-
-    for (Py_ssize_t i = 0; i < PyList_Size(p); i++) {
-        PyObject *o = PyList_GetItem(p, i);
-
-        if (PyUnicode_Check(o)) {
-            printf("Element %ld: str\n", i);
-        } else if (PyLong_Check(o)) {
-            printf("Element %ld: int\n", i);
-        } else if (PyFloat_Check(o)) {
-            printf("Element %ld: float\n", i);
-        } else if (PyTuple_Check(o)) {
-            printf("Element %ld: tuple\n", i);
-        } else if (PyList_Check(o)) {
-            printf("Element %ld: list\n", i);
-        } else {
-            printf("Element %ld: unknown\n", i);
-        }
-    }
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (elem = 0; elem < Py_SIZE(p); elem++)
+		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
 }
+
